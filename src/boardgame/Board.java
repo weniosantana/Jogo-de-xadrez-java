c341c729cabd8ded68be1920b739c50af79c5268
weniosantana <wenio.souza123@gmail.com>
2023-05-19 13:36:43 -0300

    BoardException e Programacao defensiva

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -5,7 +5,10 @@ public class Board {
 	private int columns;
 	private Piece[][] pieces;
 	public Board(int rows, int columns) {
-		super();
+
+		if(rows<1 || columns<1) {
+			throw new BoardException("Erro ao criar um tabuleiro é necessario que tenha ao menos uma linha e uma coluna");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -13,25 +16,47 @@ public class Board {
 	public int getRows() {
 		return rows;
 	}
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
+
 	public int getColumns() {
 		return columns;
 	}
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
 	
 	public Piece piece (int row, int column) {
+		
+		if(!positionExists(row, column)) {
+			throw new BoardException("Posição fora do tabuleiro");
+		}
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Posição fora do tabuleiro");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("Já existe uma peça nessa posição: " + position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
+	
+	private boolean positionExists(int row, int column) {
+		return row>=0 && row<rows && column>=0 && column<columns;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Posição fora do tabuleiro");
+		}
+		return piece(position) != null;
+		
+	}
+	
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -2,7 +2,11 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.pieces.Bishop;
+import chess.pieces.Horse;
 import chess.pieces.King;
+import chess.pieces.Pawn;
+import chess.pieces.Queen;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -30,9 +34,51 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(0,0));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(0,7));
 		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+		board.placePiece(new Bishop(board, Color.WHITE), new Position(0,2));
+		board.placePiece(new Bishop(board, Color.WHITE), new Position(0,5));
+		board.placePiece(new Horse(board, Color.WHITE), new Position(0,1));
+		board.placePiece(new Horse(board, Color.WHITE), new Position(0,6));
+		board.placePiece(new Queen(board, Color.WHITE), new Position(0,3));
+		board.placePiece(new Pawn(board, Color.WHITE), new Position(1,0));
+		board.placePiece(new Pawn(board, Color.WHITE), new Position(1,1));
+		board.placePiece(new Pawn(board, Color.WHITE), new Position(1,2));
+		board.placePiece(new Pawn(board, Color.WHITE), new Position(1,3));
+		board.placePiece(new Pawn(board, Color.WHITE), new Position(1,4));
+		board.placePiece(new Pawn(board, Color.WHITE), new Position(1,5));
+		board.placePiece(new Pawn(board, Color.WHITE), new Position(1,6));
+		board.placePiece(new Pawn(board, Color.WHITE), new Position(1,7));
+
+
+		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.placePiece(new Rook(board, Color.BLACK), new Position(7, 0));
+		board.placePiece(new Rook(board, Color.BLACK), new Position(7, 7));
+		board.placePiece(new Bishop(board, Color.BLACK), new Position(7,2));
+		board.placePiece(new Bishop(board, Color.BLACK), new Position(7,5));
+		board.placePiece(new Horse(board, Color.BLACK), new Position(7,1));
+		board.placePiece(new Horse(board, Color.BLACK), new Position(7,6));
+		board.placePiece(new Queen(board, Color.BLACK), new Position(7,3));
+		board.placePiece(new Pawn(board, Color.BLACK), new Position(6,0));
+		board.placePiece(new Pawn(board, Color.BLACK), new Position(6,1));
+		board.placePiece(new Pawn(board, Color.BLACK), new Position(6,2));
+		board.placePiece(new Pawn(board, Color.BLACK), new Position(6,3));
+		board.placePiece(new Pawn(board, Color.BLACK), new Position(6,4));
+		board.placePiece(new Pawn(board, Color.BLACK), new Position(6,5));
+		board.placePiece(new Pawn(board, Color.BLACK), new Position(6,6));
+		board.placePiece(new Pawn(board, Color.BLACK), new Position(6,7));
+
+
+
+
+
+
+
+
+
+
+
 
 	}
 }
