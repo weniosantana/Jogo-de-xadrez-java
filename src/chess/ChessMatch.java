22be1683b0203ca5add1d50dccc28dc859c3e6cd
wenio santana <wenio.souza123@gmail.com>
2023-05-25 13:57:48 -0300

    Adicionando Rainha, cavalo, bispo

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,8 +7,11 @@ import java.util.stream.Collectors;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.Bishop;
+import chess.pieces.Horse;
 import chess.pieces.King;
 import chess.pieces.Pawn;
+import chess.pieces.Queen;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -208,12 +211,12 @@ public class ChessMatch {
 		
 		//Peças Pretas
 		placeNewPiece('a' , 8, new Rook(board, Color.BLACK));
-		//placeNewPiece('b' , 8, new Horse(board, Color.BLACK));
-		//placeNewPiece('c' , 8, new Bishop(board, Color.BLACK));
-		//placeNewPiece('d' , 8, new Queen(board, Color.BLACK));
+		placeNewPiece('b' , 8, new Horse(board, Color.BLACK));
+		placeNewPiece('c' , 8, new Bishop(board, Color.BLACK));
+		placeNewPiece('d' , 8, new Queen(board, Color.BLACK));
 		placeNewPiece('e' , 8, new King(board, Color.BLACK));
-		//placeNewPiece('f' , 8, new Bishop(board, Color.BLACK));
-		//placeNewPiece('g' , 8, new Horse(board, Color.BLACK));
+		placeNewPiece('f' , 8, new Bishop(board, Color.BLACK));
+		placeNewPiece('g' , 8, new Horse(board, Color.BLACK));
 		placeNewPiece('h' , 8, new Rook(board, Color.BLACK));
 		//Peões
 		placeNewPiece('a' , 7, new Pawn(board, Color.BLACK));
@@ -230,12 +233,12 @@ public class ChessMatch {
 		
 		//Peças Brancas
 		placeNewPiece('h' , 1, new Rook(board, Color.WHITE));
-		//placeNewPiece('b' , 1, new Horse(board, Color.WHITE));
-		//placeNewPiece('c' , 1, new Bishop(board, Color.WHITE));
-		//placeNewPiece('d' , 1, new Queen(board, Color.WHITE));
+		placeNewPiece('b' , 1, new Horse(board, Color.WHITE));
+		placeNewPiece('c' , 1, new Bishop(board, Color.WHITE));
+		placeNewPiece('d' , 1, new Queen(board, Color.WHITE));
 		placeNewPiece('e' , 1, new King(board, Color.WHITE));
-		//placeNewPiece('f' , 1, new Bishop(board, Color.WHITE));
-		//placeNewPiece('g' , 1, new Horse(board, Color.WHITE));
+		placeNewPiece('f' , 1, new Bishop(board, Color.WHITE));
+		placeNewPiece('g' , 1, new Horse(board, Color.WHITE));
 		placeNewPiece('a' , 1, new Rook(board, Color.WHITE));
 		//Peões
 		placeNewPiece('a' , 2, new Pawn(board, Color.WHITE));
